c7040b9fce9b9122e99a3035d5f5383b458019c1
Raj Rajasekaran <b10872@freescale.com>
2008-06-30 20:54:45 -0500

    ENGR00081856: Add SCC2 & RNGC driver support for i.MX37.
    Added rng & sahara library files for scc2 and RNGC tests for i.MX37.
    
    Signed-off-by: Raj Rajasekaran <b10872@freescale.com>

--- rng/fsl_shw_sym.c
+++ rng/fsl_shw_sym.c
@@ -1,6 +1,6 @@
 /*
  * User Space library to access the Security hardware
- * Copyright (C) 2005-2006 written by Freescale Semiconductor
+ * Copyright (C) 2005-2008 written by Freescale Semiconductor
  *
  * This library is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -17,8 +17,10 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
  * USA
  */
-/*!
- * @file lib/rng/fsl_shw_sym.c
+
+
+/**
+ * @file fsl_shw_sym.c
  *
  * This file implements Symmetric Cipher functions of the FSL SHW API.  This
  * does not include CCM.
@@ -26,11 +28,12 @@
 
 #include "shw_driver.h"
 
+
 /* REQ-S2LRD-PINTFC-API-BASIC-SYM-002 */
 /* PINTFC-API-BASIC-SYM-ARC4-001 */
 /* PINTFC-API-BASIC-SYM-ARC4-002 */
 
-/*!
+/**
  * Compute symmetric encryption
  *
  *
@@ -43,30 +46,34 @@
  *
  * @return    A return code of type #fsl_shw_return_t.
  */
-fsl_shw_return_t fsl_shw_symmetric_encrypt(fsl_shw_uco_t * user_ctx,
-					   fsl_shw_sko_t * key_info,
-					   fsl_shw_scco_t * sym_ctx,
-					   uint32_t length,
-					   const uint8_t * pt, uint8_t * ct)
+fsl_shw_return_t fsl_shw_symmetric_encrypt(
+                                fsl_shw_uco_t* user_ctx,
+                                fsl_shw_sko_t* key_info,
+                                fsl_shw_scco_t* sym_ctx,
+                                uint32_t length,
+                                const uint8_t* pt,
+                                uint8_t* ct)
 {
-	fsl_shw_return_t ret = FSL_RETURN_ERROR_S;
+    fsl_shw_return_t ret = FSL_RETURN_ERROR_S;
 
-	/* Unused */
-	(void)user_ctx;
-	(void)key_info;
-	(void)sym_ctx;
-	(void)length;
-	(void)ct;
-	(void)pt;
 
-	return ret;
+    /* Unused */
+    (void)user_ctx;
+    (void)key_info;
+    (void)sym_ctx;
+    (void)length;
+    (void)ct;
+    (void)pt;
+
+    return ret;
 }
+
 
 /* PINTFC-API-BASIC-SYM-002 */
 /* PINTFC-API-BASIC-SYM-ARC4-001 */
 /* PINTFC-API-BASIC-SYM-ARC4-002 */
 
-/*!
+/**
  * Compute symmetric decryption
  *
  *
@@ -79,21 +86,26 @@ fsl_shw_return_t fsl_shw_symmetric_encrypt(fsl_shw_uco_t * user_ctx,
  *
  * @return    A return code of type #fsl_shw_return_t.
  */
-fsl_shw_return_t fsl_shw_symmetric_decrypt(fsl_shw_uco_t * user_ctx,
-					   fsl_shw_sko_t * key_info,
-					   fsl_shw_scco_t * sym_ctx,
-					   uint32_t length,
-					   const uint8_t * ct, uint8_t * pt)
+fsl_shw_return_t fsl_shw_symmetric_decrypt(
+                                fsl_shw_uco_t* user_ctx,
+                                fsl_shw_sko_t* key_info,
+                                fsl_shw_scco_t* sym_ctx,
+                                uint32_t length,
+                                const uint8_t* ct,
+                                uint8_t* pt)
 {
-	fsl_shw_return_t ret = FSL_RETURN_ERROR_S;
+    fsl_shw_return_t ret = FSL_RETURN_ERROR_S;
 
-	/* Unused */
-	(void)user_ctx;
-	(void)key_info;
-	(void)sym_ctx;
-	(void)length;
-	(void)ct;
-	(void)pt;
 
-	return ret;
+
+    /* Unused */
+    (void)user_ctx;
+    (void)key_info;
+    (void)sym_ctx;
+    (void)length;
+    (void)ct;
+    (void)pt;
+
+    return ret;
 }
+
