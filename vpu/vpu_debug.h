5b7d13e7e4e9feb8d5597749ca25a92dca11cd14
Robby Cai <r63905@freescale.com>
2008-07-02 15:26:43 +0800

    ENGR00081581 update i.MX37 VPU firmware to v1.0.3
    1.Bug fixes:
    ================
    1.1. H.264 Q matrix decoding bug
    In the previous version of firmware, H.264 decoder could not decode some files because of Q matrix decoding bug. In this new version, this bug has been fixed.
    
    1.2. VC-1 non-support feature checking routine bug In the previous version of firmware, VC-1 decoder didn't return fail code although the stream could not be decoded by VPU. In this new version, this bug has been fixed.
    
    2. Patches
    ================
    2.1. Modify packed PB frame in MPEG-4 stream processing method in the file play mode In the previous version of firmware, VPU decodes two frames when packed PB frame is fed by host in file play mode. It cause display buffer corruption because host can not know which buffer is over written by decoder. In this new version, VPU decodes only one frame and returns flag indicates reuse of current chunk data. So, display buffer corruption is disappeared.
    
    2.2. Add reset control register to support one instance reset In the previous version of firmware, whole internal data related with all instances is cleared after HW reset. So, in case of exception case of only one instance, all instances must be closed. In this new version, VPU don't clear internal data if dedicated register is set as 1. So, host application can control multi instance by control the register.
    
    2.3. Display buffer control by Host
    In the previous firmware, Marley control the display buffer management in the VPU decoder inside. So, although host application has not displayed target frame buffer yet, VPU decoder can overwrite the frame buffer. It can cause display corruption.
    
    So, in this new firmware, flags which indicate frame buffers have been displayed or not are newly added to support host-side display buffer management. Host application can control the buffer use by setting buffer use flag register. This new scheme will give more flexibility to host application in display point of view.
    
    Signed-off-by: Robby Cai <r63905@freescale.com>

--- vpu/vpu_debug.h
+++ vpu/vpu_debug.h
@@ -23,8 +23,26 @@
 #ifndef __VPU_DEBUG_H
 #define __VPU_DEBUG_H
 
+
 #define	DEBUG_LEVEL	0
+
 #define dprintf(level, fmt, arg...)     if (DEBUG_LEVEL >= level) \
-        printf("<VPU-lib> " fmt , ## arg)
+        printf("[DEBUG]\t%s:%d " fmt, __FILE__, __LINE__, ## arg)
+
+#define err_msg(fmt, arg...) do { if (DEBUG_LEVEL >= 1)		\
+	printf("[ERR]\t%s:%d " fmt,  __FILE__, __LINE__, ## arg); else \
+	printf("[ERR]\t" fmt, ## arg);	\
+	} while (0)
+#define info_msg(fmt, arg...) do { if (DEBUG_LEVEL >= 1)		\
+	printf("[INFO]\t%s:%d " fmt,  __FILE__, __LINE__, ## arg); else \
+	printf("[INFO]\t" fmt, ## arg);	\
+	} while (0)
+#define warn_msg(fmt, arg...) do { if (DEBUG_LEVEL >= 1)		\
+	printf("[WARN]\t%s:%d " fmt,  __FILE__, __LINE__, ## arg); else \
+	printf("[WARN]\t" fmt, ## arg);	\
+	} while (0)
+
+#define ENTER_FUNC() dprintf(4, "enter %s()\n", __func__)
+#define EXIT_FUNC() dprintf(4, "exit %s()\n", __func__)
 
 #endif
