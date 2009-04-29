3913816758829b7e2dd620d4938a56b4a2809fbf
Raj Rajasekaran <b10872@freescale.com>
2009-04-29 12:02:41 -0500

    ENGR00112017: Change RNG and Sahara library files to use LGPL headers.
    Modified to use LGPL headers.
    
    Signed-off-by: Raj Rajasekaran <b10872@freescale.com>

--- sahara2/fsl_shw_keystore.c
+++ sahara2/fsl_shw_keystore.c
@@ -1,15 +1,22 @@
 /*
- * Copyright 2008-2009 Freescale Semiconductor, Inc. All Rights Reserved.
+ * User Space library to access the Security hardware
+ * Copyright 2005-2009 Freescale Semiconductor, Inc. All Rights Reserved.
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
  */
 
-/*
- * The code contained herein is licensed under the GNU General Public
- * License. You may obtain a copy of the GNU General Public License
- * Version 2 or later at the following locations:
- *
- * http://www.opensource.org/licenses/gpl-license.html
- * http://www.gnu.org/copyleft/gpl.html
- */
 
 /**
  * @file fsl_shw_keystore.c
