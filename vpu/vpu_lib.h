cb1b7e3429db337a477563883300785ea23e1a14
Sammy He <r62914@freescale.com>
2010-07-08 11:13:23 +0800

    ENGR00124956 vpu: Enable partial internal ram usage
    Enable partial internal ram usage when iram isn't
    enough on MX51 and MX53
    
    Signed-off-by: Sammy He <r62914@freescale.com>

--- vpu/vpu_lib.h
+++ vpu/vpu_lib.h
@@ -500,7 +500,7 @@ typedef struct vpu_versioninfo {
  * v4.1.2 [2008.08.22] update MX37 VPU firmware to V1.0.5
  * v4.0.2 [2008.08.21] add the IOClkGateSet() for power saving.
  */
-#define VPU_LIB_VERSION_CODE	VPU_LIB_VERSION(5, 0, 5)
+#define VPU_LIB_VERSION_CODE	VPU_LIB_VERSION(5, 1, 0)
 
 extern unsigned int system_rev;
 
