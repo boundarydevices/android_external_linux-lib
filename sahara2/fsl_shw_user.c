a6f7471da07304ef523abb9b19d9322e51a10a2f
Anish Trivedi <anish@freescale.com>
2011-03-31 15:57:16 -0500

    ENGR00141479 [imx-lib] SAHARA: Use relocated header file
    The user mode libsahara library relied on header file
    drivers/mxc/security/sahara2/include/sahara.h. However, to make
    the lib build after headers_install step and to remove dependency on
    kernel source, this header was moved to include/linux/mxc_sahara.h.
    
    Made necessary changes to reference the relocated header file in
    imx-lib.
    
    Signed-off-by: Anish Trivedi <anish@freescale.com>

--- sahara2/fsl_shw_user.c
+++ sahara2/fsl_shw_user.c
@@ -1,6 +1,6 @@
 /*
  * User Space library to access the Security hardware
- * Copyright 2005-2009 Freescale Semiconductor, Inc. All Rights Reserved.
+ * Copyright 2005-2009, 2011 Freescale Semiconductor, Inc. All Rights Reserved.
  *
  * This library is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -24,7 +24,7 @@
  * This file implements user and platform capabilities functions of the FSL SHW
  * API for Sahara
  */
-#include "sahara.h"
+#include <linux/mxc_sahara.h>
 #include <adaptor.h>
 #include <sf_util.h>
 
