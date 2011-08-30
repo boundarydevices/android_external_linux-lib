15a21143ac77de000ede4703046a2631b9c50a6a
Xinyu Chen <b03824@freescale.com>
2011-08-30 14:11:31 +0800

    ENGR00155560 ipulib: make ipu lib compatible on 2.6.38 kernel for android
    Since android is still using ipulib to do video rendering on mx6q,
    we need ipu lib for only one ipu instance working on 2.6.38 kernel.
    So copy the 2.6.35 ipu.h header here, use linux version to distinguish
    include the <linux/ipu.h> or "ipu.h" in the code.
    Also force FG framebuffer to fb1 when we're running 2.6.38 kernel.
    Update the android release verion check routing for honeycomb.
    
    Signed-off-by: Xinyu Chen <xinyu.chen@freescale.com>

--- ipu/mxc_ipu_lib.c
+++ ipu/mxc_ipu_lib.c
@@ -35,7 +35,13 @@
 #include <sys/mman.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
+#include <linux/version.h>
+
+#if LINUX_VERSION_CODE > KERNEL_VERSION(2,6,35)
+#include "ipu.h"
+#else
 #include <linux/ipu.h>
+#endif
 
 int fd_ipu = -1;
 static int open_count = 0;
