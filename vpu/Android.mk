1701be5393a746f572dd0fd786b4b8ef6ebc89b0
Lin Fuzhen <fuzhen.lin@freescale.com>
2012-04-12 18:38:57 +0800

    ENGR00179049 mx6vpu: Add vpu support for mx6dl
    For MX6Q and MX6Dl use the same registers map, so use BOARD_SOC_CLASS
    instead of BOARD_SOC_TYPE to avoid more code changes.
    
    Signed-off-by: Lin Fuzhen <fuzhen.lin@freescale.com>

--- vpu/Android.mk
+++ vpu/Android.mk
@@ -9,7 +9,11 @@ LOCAL_SRC_FILES := \
 	vpu_util.c \
 	vpu_lib.c \
 	vpu_gdi.c
+ifeq ($(BOARD_SOC_CLASS),IMX6)
+LOCAL_CFLAGS += -DBUILD_FOR_ANDROID -DIMX6Q
+else
 LOCAL_CFLAGS += -DBUILD_FOR_ANDROID -D$(BOARD_SOC_TYPE)
+endif
 LOCAL_C_INCLUDES += $(LOCAL_PATH)
 LOCAL_SHARED_LIBRARIES := libutils libc liblog
 LOCAL_MODULE := libvpu
