884bf57a8f9c882263cdd232d48b966fcf97b9cb
Xinyu Chen <xinyu.chen@freescale.com>
2012-04-26 11:11:40 +0800

    ENGR00180865 vpu lib: Add ion allocator support
    Still keep the pmem allocator for mx5x.
    Use ion allocator for mx6 and it's successor.
    
    Signed-off-by: Xinyu Chen <xinyu.chen@freescale.com>

--- vpu/Android.mk
+++ vpu/Android.mk
@@ -14,8 +14,14 @@ LOCAL_CFLAGS += -DBUILD_FOR_ANDROID -DIMX6Q
 else
 LOCAL_CFLAGS += -DBUILD_FOR_ANDROID -D$(BOARD_SOC_TYPE)
 endif
-LOCAL_C_INCLUDES += $(LOCAL_PATH)
+ifeq ($(USE_ION_ALLOCATOR), true)
+LOCAL_CFLAGS += -DUSE_ION
+LOCAL_SHARED_LIBRARIES := libutils libc liblog libion
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../hardware/imx/ion/
+else
 LOCAL_SHARED_LIBRARIES := libutils libc liblog
+endif
+LOCAL_C_INCLUDES += $(LOCAL_PATH)
 LOCAL_MODULE := libvpu
 LOCAL_LD_FLAGS += -nostartfiles
 LOCAL_PRELINK_MODULE := false
