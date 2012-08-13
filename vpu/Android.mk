f04e004d55715d21887812eb1260df74a1ba7359
Xianzhong <b07117@freescale.com>
2012-08-13 23:15:13 +0800

    ENGR00219602 Alloc VPU memory through GPU video pool
    Use GPU video memory interface to implement VPU memory allocator,
    Unify the reserved memory for GPU & VPU to ultize memory with max efficiency
    
    Signed-off-by: Xianzhong <b07117@freescale.com>

--- vpu/Android.mk
+++ vpu/Android.mk
@@ -14,12 +14,17 @@ LOCAL_CFLAGS += -DBUILD_FOR_ANDROID -DIMX6Q
 else
 LOCAL_CFLAGS += -DBUILD_FOR_ANDROID -D$(BOARD_SOC_TYPE)
 endif
+ifeq ($(USE_GPU_ALLOCATOR), true)
+LOCAL_CFLAGS += -DUSE_GPU=1
+LOCAL_SHARED_LIBRARIES := libutils libc liblog libGAL
+else
 ifeq ($(USE_ION_ALLOCATOR), true)
 LOCAL_CFLAGS += -DUSE_ION
 LOCAL_SHARED_LIBRARIES := libutils libc liblog libion
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../hardware/imx/ion/
 else
 LOCAL_SHARED_LIBRARIES := libutils libc liblog
+endif
 endif
 LOCAL_C_INCLUDES += $(LOCAL_PATH)
 LOCAL_MODULE := libvpu
