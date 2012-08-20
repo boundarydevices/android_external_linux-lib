ff0853192b5e198f4349bebe4bbbe4aad61d9b27
Hongzhang Yang <Hongzhang.Yang@freescale.com>
2012-08-20 16:29:58 +0800

    ENGR00220732-2 Remove clk_disable in VPU driver interrupt handling
    VPU lib v5.4.5
    
    Original design is VPU lib API StartOneFrame() enables clock, and VPU
    driver disables clock after codec done interrupt has been received.
    However there are known issues of interrupt handling as below:
    - VPU interrupt handling callback is not scheduled in time causing work
      queue overflow
    - JPU done interrupt is not received because JPU issues it while JPU
      buffer empty interrupt is still being served
    - VPU finishes a frame (!vpu_IsBusy) but VPU done interrupt is not
      received
    
    All above will cause clk_disable in interrupt handling not called,
    thus VPU clock count increases by 1.
    So I plan to resolve clock unbalance issue first by removing
    clk_disable from VPU driver interrupt handling. Interrupt problem
    will not affect clock issue any longer.
    
    1.    Driver: remove clk_disable from vpu_worker_callback
    2.1.  Lib: remove clk_enable from API GetOutputInfo
    2.2.  Lib: avoid disabling VPU clock when VPU is busy in SWReset
    3.    Test: replace GetOutputInfo with SWReset in decoder_close /
          encoder_close
    
    Additionally, check valid address before freeing memory in API Close
    
    Signed-off-by: Hongzhang Yang <Hongzhang.Yang@freescale.com>

--- vpu/vpu_lib.h
+++ vpu/vpu_lib.h
@@ -667,7 +667,7 @@ typedef struct vpu_versioninfo {
  * v4.2.2 [2008.09.03] support encoder on MX51
  * v4.0.2 [2008.08.21] add the IOClkGateSet() for power saving.
  */
-#define VPU_LIB_VERSION_CODE	VPU_LIB_VERSION(5, 4, 4)
+#define VPU_LIB_VERSION_CODE	VPU_LIB_VERSION(5, 4, 5)
 
 extern unsigned int system_rev;
 
