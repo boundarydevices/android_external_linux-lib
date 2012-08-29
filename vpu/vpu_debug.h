b02954bce0a9acea0a3db4cb758a87c010ad3dd3
Xinyu Chen <xinyu.chen@freescale.com>
2012-08-29 11:01:05 +0800

    ENGR00221837-1 vpu lib: refine the android log usage
    As android 4.1 change it's LOG util API to ALOGX.
    We need to adapt it to support each android version.
    
    Signed-off-by: Xinyu Chen <xinyu.chen@freescale.com>

--- vpu/vpu_debug.h
+++ vpu/vpu_debug.h
@@ -23,11 +23,30 @@
 #ifndef __VPU_DEBUG_H
 #define __VPU_DEBUG_H
 
+#ifdef BUILD_FOR_ANDROID
+#define LOG_TAG "vpu-lib"
+#include <utils/Log.h>
+#endif
+
 extern int vpu_lib_dbg_level;
 
 #define dprintf(level, fmt, arg...)     if (vpu_lib_dbg_level >= level) \
         printf("[DEBUG]\t%s:%d " fmt, __FILE__, __LINE__, ## arg)
 
+#ifdef BUILD_FOR_ANDROID
+#ifndef ALOGE
+#define ALOGE LOGE
+#endif
+#ifndef ALOGW
+#define ALOGW LOGW
+#endif
+#ifndef ALOGI
+#define ALOGI LOGI
+#endif
+#define err_msg		ALOGE
+#define info_msg	ALOGI
+#define warn_msg	ALOGW
+#else
 #define err_msg(fmt, arg...) do { if (vpu_lib_dbg_level >= 1)		\
 	printf("[ERR]\t%s:%d " fmt,  __FILE__, __LINE__, ## arg); else \
 	printf("[ERR]\t" fmt, ## arg);	\
@@ -40,6 +59,7 @@ extern int vpu_lib_dbg_level;
 	printf("[WARN]\t%s:%d " fmt,  __FILE__, __LINE__, ## arg); else \
 	printf("[WARN]\t" fmt, ## arg);	\
 	} while (0)
+#endif
 
 #define ENTER_FUNC() dprintf(4, "enter %s()\n", __func__)
 #define EXIT_FUNC() dprintf(4, "exit %s()\n", __func__)
