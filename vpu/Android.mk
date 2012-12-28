d0fdc1070e8f1096b9b140a9f1bbc4965055115e
Hongzhang Yang <Hongzhang.Yang@freescale.com>
2012-12-28 18:39:50 +0800

    ENGR00238447 JPEG decoder bug fixes
    VPU lib v5.4.11
    
    * JPEG decoder bug fixes
    * return RETCODE_NOT_SUPPORTED for not supported small resolution
    
    Bug 1&2: customer support for Cloud Times
    Reproduced using Jpglib test app (roll back mode)
    
    - Bug 1:
    vpu_WaitForInt success but VPU is still busy
    
    Root cause:
    Receiving interrupt but status was not buffer empty was
    treated as picture done. But in some case, VPU lib could handle and
    clear buffer empty status before interrupt was received due to delay
    (VPU blocking timeout). So after receiving the delayed buffer empty,
    WaitForInterrupt returned success. The risk is to reset VPU when it's
    busy.
    
    Solution:
    Return success only if picture done or error status was set.
    
    - Bug 2:
    Green lines were displayed in each MCU row, on top/bottom MCU border
    After that, MPEG-4 or other decoders were always blocked in decoding 1st
    picture.
    Couldn't recover even after power off/on VPU and re-run test app.
    
    Root cause:
    Improper reset flow: mask new bus request -> existing request done
    -> unmask request -> reset, new request may come before reset.
    This will lead to 1) bus hang 2) abnormal bus transactions unrecoverable
    unless board reset
    Jpglib ring buffer mode has roll back in each picture, so reset is
    called.
    Jpglib line buffer mode and VPU unit test don't trigger roll back.
    
    Solution:
    Mask new request -> existing request done -> reset -> unmask request
    
    - Bug 3:
    16KB sized picture decoding may always timeout then SWReset
    (ENGR00221985)
    Reproduced using VPU wrapper test app or Gstreamer (line buffer mode)
    
    Root cause:
    Has 512 byte stuffing after EOI, so BBC interrupt and picture done
    interrupt may come at the same time for such pictures. VPU lib clears
    BBC interrupt and wait for picture done interrupt which has already been
    issued.
    
    Solution:
    check all status when interrupt comes
    
    - Bug 4:
    JPEG decoding has no output (both modes) for a 188KB sized picture
    Reproduced using VPU unit test
    
    Found a RTL BUG:
    If picture size is a multiple of 256, BBC interrupt coincides with stream
    end process, if a GBU request is queued, BBC FSM will not back to IDLE
    state, so doesn't follow stream end process. BBHR isn't set to correct
    value, so DEC FSM will stall after GBU gets the last byte
    
    Solution:
    Disable BBC interrupt after stream end, so that stream end process
    won't coincide with BBC interrupt.
    Set MJPEG_BBC_END_ADDR_REG behind MJPEG_BBC_STRM_CTRL_REG
    (unreachable position).
    
    - Handling incomplete clip in line buffer mode
    
    RTL BUG:
    Although SW passes the clip end position to JPU, JPU will still be
    blocked because no more bs comes while picture is not done
    
    Solution in line buffer mode:
    Add margin of 3 256B buffers after end of clip position
    (modify MJPEG_BBC_STRM_CTRL_REG). For incomplete clip all 3
    buffers will be consumed. For correct clip at most 2 buffers
    can be consumed. We can compare the read pointer with end
    position to differentiate.
    
    Signed-off-by: Hongzhang Yang <Hongzhang.Yang@freescale.com>

--- vpu/Android.mk
+++ vpu/Android.mk
@@ -8,7 +8,8 @@ LOCAL_SRC_FILES := \
 	vpu_io.c \
 	vpu_util.c \
 	vpu_lib.c \
-	vpu_gdi.c
+	vpu_gdi.c \
+	vpu_debug.c
 ifeq ($(BOARD_SOC_CLASS),IMX6)
 LOCAL_CFLAGS += -DBUILD_FOR_ANDROID -DIMX6Q
 else
