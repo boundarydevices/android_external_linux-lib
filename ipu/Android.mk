f1c14beda7e8ef6637df315dc0bf18d11e766d31
Liu Ying <Ying.liu@freescale.com>
2013-01-08 15:48:57 +0800

    ENGR00239212 IPU lib:Support dummy IPU lib for MX6DQ/DL
    This patch supports dummy IPU lib for MX6DQ/DL Android
    since vpu unit test may require that to pass build.
    
    Signed-off-by: Liu Ying <Ying.Liu@freescale.com>

--- ipu/Android.mk
+++ ipu/Android.mk
@@ -1,11 +1,15 @@
-ifeq ($(BOARD_SOC_CLASS),IMX5X)
 ifeq ($(HAVE_FSL_IMX_IPU),true)
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
+ifeq ($(BOARD_SOC_CLASS),IMX5X)
 LOCAL_SRC_FILES := \
-       mxc_ipu_hl_lib.c \
-       mxc_ipu_lib.c
+	mxc_ipu_hl_lib.c \
+	mxc_ipu_lib.c
+else
+LOCAL_SRC_FILES := \
+	mxc_ipu_hl_lib_dummy.c
+endif
 
 LOCAL_CFLAGS += -DBUILD_FOR_ANDROID
 
@@ -18,5 +22,4 @@ LOCAL_LD_FLAGS += -nostartfiles
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_TAGS := eng
 include $(BUILD_SHARED_LIBRARY)
-endif
 endif
